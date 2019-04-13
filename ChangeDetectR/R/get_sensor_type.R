20086e68aa35fe531438eb888e14e4a32480654f
MagdaHa <m.halbgewachs@online.de>
2019-04-13 18:08:01 +0200

    function returns the sensor type of an satellite image

--- ChangeDetectR/R/get_sensor_type.R
+++ ChangeDetectR/R/get_sensor_type.R
@@ -1,3 +1,15 @@
+#' get_sensor_type
+#'
+#' This function returns the satellite name based on the file name (only for Landsat 4, 5 and 8).
+#' output: sensor type name
+#' @param image_path uses the path where the image is located
+#' @details this function does xxx
+#' 
+#' @keywords Landsat, bands, sensor,remote sensing
+#' @export
+#' @examples
+
+
 get_sensor_type <- function(image_path) {
   
   if (grepl("LC08", image_path)){
