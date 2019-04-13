d955277150d699f812c7d1f4a0e11bdfb6d6b2cc
MagdaHa <m.halbgewachs@online.de>
2019-04-13 17:56:42 +0200

    function for calulating the NDWI

--- ChangeDetectR/R/NDWI.R
+++ ChangeDetectR/R/NDWI.R
@@ -1,5 +1,17 @@
-#calculating NDWI for each image
-#function NDWI
+#' NDWI
+#'
+#' This function calculates the NDWI (Normaized Difference Vegetation Index) for Satellite image
+#' output: raster layer
+#' @param green green band as input
+#' @param nir nir band as input
+
+#' 
+#' @details this function does xxx
+#' @keywords Landsat, bands, NDWI, water index, remote sensing
+#' @export
+#' @examples
+
+
 NDWI <- function(green,nir) {
   ndwi <- (green-nir)/(green+nir)
   return(ndwi)
