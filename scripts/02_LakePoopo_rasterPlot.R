57f44c9afd955536468614d17a0c6b2864208a75
MagdaHa <m.halbgewachs@online.de>
2019-04-14 12:56:47 +0200

    aesthetic corrections

--- scripts/02_LakePoopo_rasterPlot.R
+++ scripts/02_LakePoopo_rasterPlot.R
@@ -1,62 +1,76 @@
-######################
-#### Raster Plots ####
-######################
+##########################################################################################
+#### Lake Poopo, Bolivia: Raster Plots
+#### author: Magdalena Halbgewachs
+#### April 2019
+##########################################################################################
 
+##########################################################################################
+#### Background information
+
+### Study area: Lake Poopó, Bolivia
+### content:
+### 1.) Plot NDWI images
+### 2.) Plot classified (binary) images and bathymetric lines
+### 3.) Plot Change Detection
+
+##########################################################################################
+
+#loading required packages
 library(ggplot2)
 library(rgdal)
 library(raster)
 library(rasterVis)
 library(maptools)
 
-######################################################################################
-################################## NDWI ##############################################
-######################################################################################
+##########################################################################################
+# 1.) NDWI 
+##########################################################################################
 NDWI <- "D:/01_Uni/02_Master/MB1_Digital Image Analysis and GIS/00_final_project/01_Landsat/NDWI"
 all_NDWI <- list.files(NDWI, full.names = TRUE, pattern = ".tif$")
 all_NDWI
 
-#------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
 # Create a raster stack of the NDWI time series
 NDWI_stack <- stack(all_NDWI)
 # view names for each raster layer
 names(NDWI_stack)
-# use gsub to modify label names
+# gsub to modify label names
 rasterNames  <- gsub("NDWI_","", names(NDWI_stack))
 
-#----------------------------------------------------------------------
-#levelplot with one header and legend
-cols <- colorRampPalette(brewer.pal(11,"Spectral"))    # number of colors (9 maybe better)
+#----------------------------------------------------------------------------------------
+#levelplot
+cols <- colorRampPalette(brewer.pal(11,"Spectral"))    #  number of colors
 
 levelplot(NDWI_stack,
           main="NDWI\nLake Poopó, Bolivia (1989 - 2018)",
           col.regions=cols,
-          names.attr=rasterNames,      # using new defined names
-          scales=list(draw=FALSE )) # remove axes labels & ticks
+          names.attr=rasterNames,                     # using new defined names
+          scales=list(draw=FALSE ))                  # remove axes labels & ticks
 
 
-######################################################################################
-######################### CLASSIFICATION AND BATHYMETRY ##############################
-######################################################################################
+##########################################################################################
+# 2.) CLASSIFICATION AND BATHYMETRY 
+##########################################################################################
 class <- "D:/01_Uni/02_Master/MB1_Digital Image Analysis and GIS/00_final_project/01_Landsat/classification"
 all_class <- list.files(class, full.names = TRUE, pattern = ".tif$")
 all_class
 
-#------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
 # Create a raster stack of the NDWI time series
 class_stack <- stack(all_class)
 # view names for each raster layer
 names(class_stack)
-# use gsub to modify label names.that we'll use for the plot 
+# gsub to modify label names
 rasterNames  <- gsub("class_","", names(class_stack))
 
-#------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
 #load bathymetry lines
 bath.shp <- readOGR("D:\\01_Uni\\02_Master\\MB1_Digital Image Analysis and GIS\\00_final_project\\Lake_Poopo\\bathymetry_qgis.shp")
 crs(bath.shp)
 crs(class_stack)
 
-#----------------------------------------------------------------------
-#levelplot with one header and legend
+#----------------------------------------------------------------------------------------
+#levelplot
 cols <- rasterTheme(region = brewer.pal("Blues", n=3))
 cuts <- c(0.1, 0.5, 1.1)
 
@@ -64,43 +78,43 @@ levelplot(class_stack,
           main="Lake area\nLake Poopó, Bolivia (1989 - 2018)",
           par.settings=cols,
           at=cuts,
-          names.attr=rasterNames, # using new defined names
+          names.attr=rasterNames,
           kolorkey=F,
-          scales=list(draw=FALSE ))+ # remove axes labels & ticks
+          scales=list(draw=FALSE ))+
   layer(sp.polygons(bath.shp), packets = (1:22))
 
 
-######################################################################################
-############################# CHANGE DETECTION: MINUS ################################
-######################################################################################
+##########################################################################################
+# 3.) CHANGE DETECTION
+##########################################################################################
 minus <- "D:/01_Uni/02_Master/MB1_Digital Image Analysis and GIS/00_final_project/01_Landsat/change_minus"
 all_minus <- list.files(minus, full.names = TRUE, pattern = ".tif$")
 all_minus
 
-#------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
 # Create a raster stack of the NDWI time series
 minus_stack <- stack(all_minus)
 # view names for each raster layer
 names(minus_stack)
-# use gsub to modify label names.that we'll use for the plot 
+# gsub to modify label names
 rasterNames  <- gsub("change_","", names(minus_stack))
 
-#----------------------------------------------------------------------
-#levelplot with one header and legend
-cols <- colorRampPalette(brewer.pal(9,"RdBu"))    # number of colors
+#----------------------------------------------------------------------------------------
+#levelplot
+cols <- colorRampPalette(brewer.pal(9,"RdBu"))
 
 levelplot(minus_stack,
           main="Loss and gain of water area\nLake Poopó, Bolivia (1989 - 2018)",
           col.regions=cols,
-          names.attr=rasterNames,      # using new defined names
+          names.attr=rasterNames,
           #col=cols,
           #col = c("white", "blue"),
           #par.settings=GrTheme(),
-          scales=list(draw=FALSE )) # remove axes labels & ticks
+          scales=list(draw=FALSE ))
 
 
-
-#-----------------------------------------
+########################################################################################
+#----------------------------------------------------------------------------------------
 #animation
 urb_anim <- tm_shape(bath.shp)+tm_polygons(bath.shp) +
   tm_raster(class_stack$class_1989_04)+
