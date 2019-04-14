38c48804b02fe2ac70770f21ef98a63ef85fb039
MagdaHa <m.halbgewachs@online.de>
2019-04-14 12:55:59 +0200

    skript which shows the investigation area on a map

--- scripts/00_LakePoopo_investigation_area.R
+++ scripts/00_LakePoopo_investigation_area.R
@@ -1,12 +1,36 @@
-#------------geht http://keithnewman.co.uk/r/maps-in-r.html
-library(maps)       # Provides functions that let us plot the maps
+##########################################################################################
+#### Lake Poopo, Bolivia: Investigation area
+#### author: Magdalena Halbgewachs
+#### April 2019
+##########################################################################################
+
+### inspiration: http://keithnewman.co.uk/r/maps-in-r.html
+
+##########################################################################################
+#### Background information
+
+### Study area: Lake Poopó, Bolivia
+
+##########################################################################################
+
+#loading required packages
+library(maps)
 library(mapdata)
-map('worldHires','Bolivia', col = 'grey')
-points(-67.056825,-18.735300,col=2,pch=16)
+
+#----------------------------------------------------------------------------------------
+#mapping, version 1
+map('worldHires','Bolivia', col = 'grey', fill=T)
 title('Lake Poopó, Bolivia')
+points(-67.056825,-18.735300,col= 'blue',pch=16, size=2)
 map('lakes', add=T, fill=T, col = 'blue', boundary='blue')
+#map('rivers', add=T)
 
-
-map('rivers', add=T)
+#----------------------------------------------------------------------------------------
+#mapping, version 2
+map('worldHires',
+    c('Bolivia', 'Peru'), col='grey', fill=T)
+title('Lake Poopó, Bolivia')
+points(-67.056825,-18.735300,col= 'blue',pch=16, size=2)
+map('lakes', add=T, fill=T, col = 'blue', boundary='blue')
 
 
