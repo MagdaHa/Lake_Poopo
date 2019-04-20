21523b12720aa422e6758b76461147ce45d81712
MagdaHa <m.halbgewachs@online.de>
2019-04-20 11:43:05 +0200

    simplification of data import

--- scripts/03_LakePoopo_ggplots.R
+++ scripts/03_LakePoopo_ggplots.R
@@ -31,57 +31,24 @@ library(corrplot)
 ##########################################################################################
 # 1.) data import
 ##########################################################################################
-
-###precipitation###
-
-#all months 1989 - 2018
-prec_mean_all <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\prec_mean.csv", header=T, sep=";")
-if(names(prec_mean_all)[1]=="ï..YEAR"){
-  names(prec_mean_all)[1]<- "YEAR"
+###precipitation and evapotranspiration all months 1989 - 2018###
+df_data_all <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\prec_et_mean_all.csv", header=T, sep=";")
+if(names(df_data_all)[1]=="ï..YEAR"){             #replaces mysterious column name
+  names(df_data_all)[1]<- "YEAR"
 }
-prec_mean_all[[1]] <- as.Date(prec_mean_all[[1]])   #read as date
-
-#only April 1989 - 2018
-prec_mean_april <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\precipitation_mean_date_april.csv", header=T, sep=";")
-if(names(prec_mean_april)[1]=="ï..YEAR"){
-  names(prec_mean_april)[1]<- "YEAR"
-}
-prec_mean_april[[1]] <- as.Date(prec_mean_april[[1]])
-
-#only July 1989 - 2018
-prec_mean_july <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\precipitation_mean_date_july.csv", header=T, sep=";")
-if(names(prec_mean_july)[1]=="ï..YEAR"){
-  names(prec_mean_july)[1]<- "YEAR"
-}
-prec_mean_july[[1]] <- as.Date(prec_mean_july[[1]])
+df_data_all[[1]] <- as.Date(df_data_all[[1]], origin="1989-01.01")   #read as date
+names(df_data_all) <- c ("YEAR", "PREC", "ET")  #rename columns
 
 #----------------------------------------------------------------------------------------
-
-###evapotranspiration###
-
-#all months 1989 - 2018
-et_mean_all <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\ET_mean.csv", header=T, sep=";")
-if(names(et_mean_all)[1]=="ï..YEAR"){
-  names(et_mean_all)[1]<- "YEAR"
+###precipitation and evapotranspiration April and July 1989 - 2018###
+df_data <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\prec_et_april_july.csv", header=T, sep=";")
+if(names(df_data)[1]=="ï..YEAR"){             #replaces mysterious column name
+  names(df_data)[1]<- "YEAR"
 }
-et_mean_all[[1]] <- as.Date(et_mean_all[[1]])
-
-#only April 1989 - 2018
-et_mean_april <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\ET_mean_date_april.csv", header=T, sep=";")
-if(names(et_mean_april)[1]=="ï..YEAR"){
-  names(et_mean_april)[1]<- "YEAR"
-}
-et_mean_april[[1]] <- as.Date(et_mean_april[[1]])
-
-#only July 1989 - 2018
-et_mean_july <- read.csv("C:\\02_Studium\\02_Master\\01_Semester 1\\00_paper_work\\01_Lakes\\Lake_Poopó\\ET_mean_date_july.csv", header=T, sep=";")
-if(names(et_mean_july)[1]=="ï..YEAR"){
-  names(et_mean_july)[1]<- "YEAR"
-}
-et_mean_july[[1]] <- as.Date(et_mean_july[[1]])
+df_data[[1]] <- as.Date(df_data[[1]], origin="01-04-1989")   #read as date
+names(df_data) <- c ("YEAR", "PREC_APRIL", "PREC_JULY", "ET_APRIL", "ET_JULY")
 
 #----------------------------------------------------------------------------------------
-
 ###lake area###
 
 #only April 1989 - 2018 (based on df of index calculations)
@@ -89,7 +56,7 @@ area_april <- area_df_april
 if(names(area_april)[1]=="ï..YEAR"){
   names(area_april)[1]<- "YEAR"
 }
-area_april[[1]] <- as.Date(area_april[[1]], origin="1989-04-1")
+area_april[[1]] <- as.Date(area_april[[1]], origin="1989-04-01")
 
 #only July 1989 - 2018 (based on df of index calculations)
 area_july <- area_df_july
@@ -116,9 +83,14 @@ area_july[[1]] <- as.Date(area_july[[1]], origin="1989-07-1")
 #}
 #area_july[[1]] <- as.Date(area_july[[1]], origin="1989-07-1")
 
+#----------------------------------------------------------------------------------------
+###save all data from April and July in one dataframe###
+df_data <- merge (df_data, area_april,by="YEAR", all=T)
+df_data <- merge (df_data, area_july, by="YEAR", all=T)
+names(df_data) <- c ("YEAR", "PREC_APRIL", "PREC_JULY", "ET_APRIL", "ET_JULY", "A_APRIL", "A_JULY")
+
 
 #----------------------------------------------------------------------------------------
-
 ###lake area Titicaca###
 
 titicaca <- read.table("D:\\01_Uni\\02_Master\\MB1_Digital Image Analysis and GIS\\00_final_project\\Titicaca\\time_series_titicaca", sep = "", header = T, fill = T)
@@ -126,22 +98,6 @@ if(names(titicaca)[1]=="ï..YEAR"){
   names(titicaca)[1]<- "YEAR"
 }
 titicaca[[1]] <- as.Date(titicaca[[1]])
-
-#----------------------------------------------------------------------------------------
-
-###save all data in one dataframe###
-
-#all April and July data
-df_data <- merge (area_april, area_july,by="YEAR", all=T)
-df_data <- merge (df_data, prec_mean_april, by="YEAR", all=T)
-df_data <- merge (df_data, prec_mean_july, by="YEAR", all=T)
-df_data <- merge (df_data, et_mean_april, by="YEAR", all=T)
-df_data <- merge (df_data, et_mean_july, by="YEAR", all=T)
-names(df_data) <- c ("YEAR", "A_APRIL", "A_JULY", "PREC_APRIL", "PREC_JULY", "ET_APRIL", "ET_JULY")
-
-#all months 1989 - 2018
-df_data_all <- merge (et_mean_all, prec_mean_all,by="YEAR", all=T)
-names(df_data_all) <- c ("YEAR", "ET", "PREC")
 
 
 ################################################################################
@@ -162,6 +118,8 @@ ggplot(data=df_data_all, aes(x=YEAR, y=PREC, group=1, color=legend)) +
   #axis
   xlab("year")+
   ylab("precipitation (mm)")+
+  scale_x_date(breaks = as.Date(c("1989-04-01", "1994-04-01", "1999-04-01",
+                                  "2004-04-01", "2009-04-01", "2014-04-01","2018-04-01")),date_labels = "%Y")+
   ggtitle("Mean precipitation in the Lake Poopó area (1989-2018)")+
   labs(caption = "data source: Servicio Nacional de Meteorología e Hidrología de Bolivia")+
   theme_gray(base_size = 15) 
@@ -170,8 +128,8 @@ ggplot(data=df_data_all, aes(x=YEAR, y=PREC, group=1, color=legend)) +
 #precipitation only April and July 1989 to 2019
 prec_plot <- ggplot(data=df_data, aes(x=YEAR, y=PREC_APRIL, group=1, color=legend)) +
   #data
-  geom_bar(data=df_data, aes(x=YEAR, y=PREC_APRIL, color="April"),  stat="identity", alpha=0.3, width=300, fill="transparent", size=1) +#color="orangered4"
-  geom_bar(data=df_data, aes(x=YEAR, y=PREC_JULY, color="July"),  stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+#color="aquamarine4",
+  geom_bar(data=df_data, aes(x=YEAR, y=PREC_APRIL, color="April"),  stat="identity", alpha=0.3, width=300, fill="transparent", size=1) +
+  geom_bar(data=df_data, aes(x=YEAR, y=PREC_JULY, color="July"),  stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+
   #linear regression
   geom_smooth(aes(x=YEAR, y=PREC_APRIL), formula=y~x, method='lm', color="#F8766D", linetype="dashed",  se=F)+
   geom_smooth(aes(x=YEAR, y=PREC_JULY), formula=y~x, method='lm', color="#00BFC4", linetype="dashed", se=F)+
@@ -187,7 +145,6 @@ prec_plot <- ggplot(data=df_data, aes(x=YEAR, y=PREC_APRIL, group=1, color=legen
                                   "2005-04-01", "2009-04-01", "2013-04-01",
                                   "2014-04-01", "2015-04-01", "2016-04-01", 
                                   "2017-04-01","2018-04-01")),date_labels = "%Y")+
-  #scale_fill_manual("legend", values = c("A" = "black", "B" = "orange"))+
   ggtitle("Mean precipitation in the Lake Poopó area in April and July (1989-2018)")+
   labs(caption = "data source: Servicio Nacional de Meteorología e Hidrología de Bolivia")+
   theme_gray(base_size = 15)
@@ -201,7 +158,7 @@ prec_plot
 #evapotranspiratoin all months between 1989 and 2018
 ggplot(data=df_data_all, aes(x=YEAR, y=ET, group=1, color=legend)) +
   #data
-  geom_bar(data=df_data_all, aes(x=YEAR, y=ET, color="evapotranspiration"), stat="identity", alpha=0.7, fill="red")+ #color="orangered4"
+  geom_bar(data=df_data_all, aes(x=YEAR, y=ET, color="evapotranspiration"), stat="identity", alpha=0.7, fill="red")+
   #linear regression
   geom_smooth(method='lm',formula=y~x, color="red", linetype="dashed", se=F)+
   #themes
@@ -212,16 +169,18 @@ ggplot(data=df_data_all, aes(x=YEAR, y=ET, group=1, color=legend)) +
   #axis
   xlab("year")+
   ylab("evapotranspiration (mm)")+
+  scale_x_date(breaks = as.Date(c("1989-04-01", "1994-04-01", "1999-04-01",
+                                  "2004-04-01", "2009-04-01", "2014-04-01","2018-04-01")),date_labels = "%Y")+
+  ggtitle("Mean precipitation in the Lake Poopó area (1989-2018)")+
   ggtitle("Mean evapotranspiration in the Lake Poopó area (1989-2018)")+
   labs(caption = "data source: Servicio Nacional de Meteorología e Hidrología de Bolivia")+
   theme_gray(base_size = 15)
 
 #----------------------------------------------------------------------------------------
-
 #evapotranspiration only April and July 1989 to 2018
 et_plot <- ggplot(data=df_data, aes(x=YEAR, y=ET_APRIL, group=1, color=legend)) +
   #data
-  geom_bar(data=df_data, aes(x=YEAR, y=ET_APRIL, color="April"), stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+ #color="orangered4",
+  geom_bar(data=df_data, aes(x=YEAR, y=ET_APRIL, color="April"), stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+
   geom_bar(data=df_data, aes(x=YEAR, y=ET_JULY, color="July"), stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+
   #linear regression
   geom_smooth(data=df_data, aes(x=YEAR, y=ET_APRIL), method='lm',formula=y~x, color="#F8766D", linetype="dashed", se=F)+
@@ -251,11 +210,11 @@ et_plot
 #water area in April and July 1989 to 2019
 water_plot <- ggplot(data=df_data, aes(x=YEAR, y=A_APRIL, group=1, color=legend)) +
   #data
-  geom_bar(data=df_data, aes(x=YEAR, y=A_APRIL, color="April"), stat="identity", alpha=0.2, width=300, fill="transparent", size=1)+ #color="orangered4"
-  geom_bar(data=df_data, aes(x=YEAR, y=A_JULY, color="July"), stat="identity", alpha=0.2, width=300, fill="transparent", size=1)+ #color="aquamarine4"
+  geom_bar(data=df_data, aes(x=YEAR, y=A_APRIL, color="April"), stat="identity", alpha=0.2, width=300, fill="transparent", size=1)+
+  geom_bar(data=df_data, aes(x=YEAR, y=A_JULY, color="July"), stat="identity", alpha=0.2, width=300, fill="transparent", size=1)+
   #linear regression
   geom_smooth(data=df_data, aes(x=YEAR, y=A_APRIL), method='lm',formula=y~x, color="#F8766D", linetype="dashed", se=F)+
-  geom_smooth(data=df_data, aes(x=YEAR, y=A_JULY), method='lm',formula=y~x, color="#00BFC4", linetype="dashed", se=F)+ #linear regression
+  geom_smooth(data=df_data, aes(x=YEAR, y=A_JULY), method='lm',formula=y~x, color="#00BFC4", linetype="dashed", se=F)+
   #themes
   theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 15),
         legend.title = element_text(size = 10, face = "bold"),
